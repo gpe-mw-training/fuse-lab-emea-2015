0cf319bbaddda81ca2bda7be53aa8b17e12a4467
Charles Moulliard <ch007m@gmail.com>
2015-08-25 14:48:20 +0200

    Add modifications for 6.1-fuse-redhat-379

--- src/main/fabric8/Readme.md
+++ src/main/fabric8/Readme.md
@@ -9,6 +9,14 @@ HTTPie request
 http POST http://127.0.0.1:9191/entries/new < src//data/entry.json
 
 
-fabric:container-remove-profile lab gpe-fuse
+
+
+fabric:create --clean root
+
+fabric:container-create-child --profile insight-elasticsearch.datastore root elasticsearch-node
+fabric:container-create-child --profile insight-core root elasticsearch-node
+fabric:container-create-child --profile feature-camel root lab
+
 fabric:container-add-profile lab gpe-fuse
 
+fabric:container-remove-profile lab gpe-fuse
--- src/main/java/com/redhat/gpe/route/ElasticSearchService.java
+++ src/main/java/com/redhat/gpe/route/ElasticSearchService.java
@@ -1,32 +1,30 @@
 package com.redhat.gpe.route;
 
 import com.redhat.gpe.model.Blog;
-import org.apache.camel.CamelContext;
-import org.apache.camel.PropertyInject;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.jackson.JacksonDataFormat;
-import org.apache.camel.component.properties.PropertiesComponent;
-import org.apache.camel.model.rest.RestBindingMode;
+//import org.apache.camel.model.rest.RestBindingMode;
 
 public class ElasticSearchService extends RouteBuilder {
-
-    // @PropertyInject("clustername")
-    // String clustername;
     
     @Override
     public void configure() throws Exception {
         
         JacksonDataFormat jacksonFormat = new JacksonDataFormat(Blog.class);
 
+        /*
         restConfiguration().component("jetty").host("0.0.0.0").port("9191").bindingMode(RestBindingMode.json);
 
         rest("/entries/")
             .post("new").consumes("application/json").type(Blog.class)
             .to("direct:add");
+            */
 
-        from("direct:add")
-            .marshal(jacksonFormat)
-            .to("elasticsearch://{{clustername}}?operation=INDEX&ip={{address}}&indexName={{indexname}}&indexType={{indextype}}")
+        from("timer://test?period=10000")
+            /* from("direct://add")    
+                .marshal(jacksonFormat) */
+                .log("Version3")
+                //.to("elasticsearch://{{clustername}}?operation=INDEX&ip={{address}}&indexName={{indexname}}&indexType={{indextype}}")
             .log("Response received : ${body}");
         
     }
